4fd2cf347993e135c04d83e8ae499b14dbc0db33
Matthew Lonis <mattlonis@gmail.com>
2023-08-17 00:15:07 -0400

    Updating to latest typescript-eslint

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,5 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000);
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error(err);
+  });
